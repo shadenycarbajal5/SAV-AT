456e719aeb566ae0fcc6e6235dc76223d11c40d5
Alexander Barrios <232158@unamba.edu.pe>
2026-06-24 18:11:58 -0500

    Agregando ResponseCajaGetAll parte 1

--- savat/src/main/java/com/epiis/savat/dto/response/ResponseCajaGetAll.java
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseCajaGetAll.java
@@ -1,5 +1,21 @@
 package com.epiis.savat.dto.response;
 
-public class ResponseCajaGetAll {
-    
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseCajaGetAll extends ResponseGeneric {
+
+    public List<Item> data;
+
+    public static class Item {
+        public Integer idCaja;
+        public LocalDateTime fechaApertura;
+        public LocalDateTime fechaCierre;
+        public BigDecimal montoInicial;
+        public BigDecimal montoFinal;
+        public Boolean abierta;
+    }
 }
