ad894649c9ed418ebfd796f4dc366102ce11d80b
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:43:03 -0500

    Agregar RepositorioRepuesttosInsert

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseRepuestoOsInsert.java
@@ -0,0 +1,7 @@
+package com.epiis.savat.dto.response;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseRepuestoOsInsert extends ResponseGeneric {
+    public Integer idRepuestoOs;
+}
