a71cf9491aa63d54ef7d0b09c83241ba9dde94cf
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:45:25 -0500

    Agregar ReposponseVentaInser

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseVentaInsert.java
@@ -0,0 +1,10 @@
+package com.epiis.savat.dto.response;
+
+import java.math.BigDecimal;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseVentaInsert extends ResponseGeneric {
+    public Integer idVenta;
+    public BigDecimal total;
+}
