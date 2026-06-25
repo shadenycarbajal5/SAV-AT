6713fef892a33f86c148251a9c9cf7fca5c9f5f2
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:35:51 -0500

    Agregar RepositorioProductoInsert

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseProductoInsert.java
@@ -0,0 +1,7 @@
+package com.epiis.savat.dto.response;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseProductoInsert extends ResponseGeneric {
+    public Integer idProducto;
+}
