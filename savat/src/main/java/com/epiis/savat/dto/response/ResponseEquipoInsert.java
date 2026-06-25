ab7cc19f0f4e595b27b525d1bab0a6fdd4a002e7
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:27:52 -0500

    Agregar RepositorioGetAll

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseEquipoInsert.java
@@ -0,0 +1,7 @@
+package com.epiis.savat.dto.response;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseEquipoInsert extends ResponseGeneric {
+    public Integer idEquipo;
+}
