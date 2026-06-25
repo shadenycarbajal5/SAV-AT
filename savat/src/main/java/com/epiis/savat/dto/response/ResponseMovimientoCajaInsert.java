a8f049680d3efb660fb5db007081bc027ab3e167
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:32:15 -0500

    Agregar RepositorioCajaInsert

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseMovimientoCajaInsert.java
@@ -0,0 +1,7 @@
+package com.epiis.savat.dto.response;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseMovimientoCajaInsert extends ResponseGeneric {
+    public Integer idMovimiento;
+}
