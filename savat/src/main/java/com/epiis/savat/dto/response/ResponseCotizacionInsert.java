f5a54546303d86dbbd9c0b667313cdc2335f73e4
Alexander Barrios <232158@unamba.edu.pe>
2026-06-25 12:26:13 -0500

    Agregar cotizacion y equiposgetall

---
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseCotizacionInsert.java
@@ -0,0 +1,10 @@
+package com.epiis.savat.dto.response;
+
+import java.math.BigDecimal;
+
+import com.epiis.savat.generic.ResponseGeneric;
+
+public class ResponseCotizacionInsert extends ResponseGeneric {
+    public Integer idCotizacion;
+    public BigDecimal total;
+}
