3c3a5934fe2d74cb9efab187f1cf5beba37a0ed2
JUAN Aroni <221151@unamba.edu.pe>
2026-06-25 22:26:27 -0500

    MOdificando  requesetusuario

--- backend/savat/savat/src/main/java/com/epiis/savat/dto/request/RequestUsuarioLogin.java
+++ backend/savat/savat/src/main/java/com/epiis/savat/dto/request/RequestUsuarioLogin.java
@@ -1,5 +1,14 @@
 package com.epiis.savat.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RequestUsuarioLogin {
-    
+
+    @NotBlank(message = "El nombre de usuario o correo electrónico es obligatorio.")
+    private String username;
+
+    @NotBlank(message = "La contraseña no puede estar vacía.")
+    private String password;
 }
