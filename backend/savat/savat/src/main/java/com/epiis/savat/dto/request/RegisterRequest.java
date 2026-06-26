6e72dc6120d2b508272ae3d770c18e39a570eca4
JUAN Aroni <221151@unamba.edu.pe>
2026-06-25 22:27:54 -0500

    MOdificando  registerrequeset

--- backend/savat/savat/src/main/java/com/epiis/savat/dto/request/RegisterRequest.java
+++ backend/savat/savat/src/main/java/com/epiis/savat/dto/request/RegisterRequest.java
@@ -1,5 +1,25 @@
 package com.epiis.savat.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio.")
+    @Size(min = 4, max = 20, message = "El usuario debe tener entre 4 y 20 caracteres.")
+    private String username;
+
+    @NotBlank(message = "El correo electrónico es obligatorio.")
+    @Email(message = "Debe proporcionar un formato de correo válido.")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria.")
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres.")
+    private String password;
     
+    @NotBlank(message = "El rol es obligatorio.")
+    private String role; // Ejemplo: "ADMIN", "USER", "CAJERO"
 }
