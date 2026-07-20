ff6affc398c070e1c56802b428411620ed96b56a
shadenycarbajal5 <shadenycarbajal5@gmail.com>
2026-07-20 11:35:32 -0500

    Add clienteDniRuc field to ResponseVentaGetAll.Item
    Added 'clienteDniRuc' field to Item class and updated constructor.

--- savat/src/main/java/com/epiis/savat/dto/response/ResponseVentaGetAll.java
+++ savat/src/main/java/com/epiis/savat/dto/response/ResponseVentaGetAll.java
@@ -1,15 +1,10 @@
 package com.epiis.savat.dto.response;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-
 import com.epiis.savat.generic.ResponseGeneric;
-
 public class ResponseVentaGetAll extends ResponseGeneric {
-
     public List<Item> data;
-
     public static class Item {
         public Integer idVenta;
         public LocalDateTime fecha;
@@ -19,12 +14,12 @@ public class ResponseVentaGetAll extends ResponseGeneric {
         public String metodoPago;
         public Integer idCliente;
         public String clienteNombre;
+        public String clienteDniRuc; // NUEVO
         public Integer idUsuario;
         public String usuarioNombre;
-
         public Item(Integer idVenta, LocalDateTime fecha, BigDecimal subtotal, BigDecimal descuento,
                      BigDecimal total, String metodoPago, Integer idCliente, String clienteNombre,
-                     Integer idUsuario, String usuarioNombre) {
+                     String clienteDniRuc, Integer idUsuario, String usuarioNombre) {
             this.idVenta = idVenta;
             this.fecha = fecha;
             this.subtotal = subtotal;
@@ -33,6 +28,7 @@ public class ResponseVentaGetAll extends ResponseGeneric {
             this.metodoPago = metodoPago;
             this.idCliente = idCliente;
             this.clienteNombre = clienteNombre;
+            this.clienteDniRuc = clienteDniRuc; // NUEVO
             this.idUsuario = idUsuario;
             this.usuarioNombre = usuarioNombre;
         }
